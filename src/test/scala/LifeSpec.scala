736a8b051433debe1c34a30f208890ea44c096e3
Steven Bakhtiari <steven.bakh@gmail.com>
2015-06-27 18:05:35 +0100

    Write more test cases

--- src/test/scala/LifeSpec.scala
+++ src/test/scala/LifeSpec.scala
@@ -11,7 +11,7 @@ class LifeSpec extends FunSpec with Matchers {
 
   val life = new Life()
 
-  describe("For any given tick") {
+  describe("For any single tick") {
     describe("when there are no living cells") {
       it("none will come to life") {
         life.tick(Set.empty) shouldBe empty
@@ -32,18 +32,11 @@ class LifeSpec extends FunSpec with Matchers {
       }
     }
 
-    describe("when there are three living cells") {
-      describe("adjacent to one another") {
-        val cells = Set(life.Cell(2, 3), life.Cell(3, 3), life.Cell(4, 3))
-        val result = life.tick(cells)
-
-        it("two of the cells die and two new cells are born") {
-          (cells diff result) should have size 2
-        }
-
-        it("there will still be a total of three cells") {
-          result should have size 3
-        }
+    describe("when a candidate position has three living neighbours") {
+      it("a new cell will come to life at that position") {
+        val cells = Set(life.Cell(2, 1), life.Cell(1, 2), life.Cell(2, 2))
+        val tick1 = life.tick(cells)
+        tick1 should contain (life.Cell(1, 1))
       }
     }
 
@@ -60,7 +53,7 @@ class LifeSpec extends FunSpec with Matchers {
         }
       }
 
-      describe("and when there are cells that form the 'beehive' pattern") {
+      describe("likewise, when there are cells that form the 'beehive' pattern") {
         it("none of the cells die and no new cells are born") {
           val beehive = Set(
             life.Cell(3, 2), life.Cell(4, 2),
@@ -69,6 +62,58 @@ class LifeSpec extends FunSpec with Matchers {
 
           val result = life.tick(beehive)
           beehive shouldEqual result
+        }
+      }
+    }
+  }
+
+  describe("When multiple ticks occur") {
+    describe("some patterns will oscillate between several states") {
+      describe("so when the 'blinker' pattern emerges") {
+        val start = Set(life.Cell(2, 3), life.Cell(3, 3), life.Cell(4, 3))
+        val expected = Set(life.Cell(3, 2), life.Cell(3, 3), life.Cell(3, 4))
+        val tick1 = life.tick(start)
+        val tick2 = life.tick(tick1)
+
+        it("the first tick will produce three live cells") {
+          tick1 should have size 3
+        }
+
+        it("where two of the cells die and two new cells are born") {
+          (start diff tick1) should have size 2
+        }
+
+        it("and the pattern matches the expected oscillation") {
+          tick1 shouldEqual expected
+        }
+
+        it("then the second tick returns cells in their original positions") {
+          tick2 shouldEqual start
+        }
+      }
+
+      describe("or if the 'toad' pattern emerges") {
+        val start = Set(
+          life.Cell(3, 3), life.Cell(4, 3), life.Cell(5, 3),
+          life.Cell(2, 4), life.Cell(3, 4), life.Cell(4, 4))
+
+        val expected = Set(
+          life.Cell(4, 2), life.Cell(2, 3), life.Cell(5, 3),
+          life.Cell(2, 4), life.Cell(5, 4), life.Cell(3, 5))
+
+        val tick1 = life.tick(start)
+        val tick2 = life.tick(tick1)
+
+        it("the first tick will produce six live cells") {
+          tick1 should have size 6
+        }
+
+        it("and the pattern matches the expected oscillation") {
+          tick1 shouldEqual expected
+        }
+
+        it("then the second tick returns cells in their original positions") {
+          tick2 shouldEqual start
         }
       }
     }
