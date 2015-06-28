0db56b2ef73dba75815632a7e4d8ab2f25c865aa
Steven Bakhtiari <steven.bakh@gmail.com>
2015-06-28 12:28:50 +0100

    Remove superfluous position case class

--- src/main/scala/Life.scala
+++ src/main/scala/Life.scala
@@ -1,35 +1,32 @@
 
 class Life {
 
-  private case class Position(x: Long, y: Long)
-
   case class Cell(x: Long, y: Long)
 
-  private def candidatePositions(cells: Set[Cell]): Set[Position] =
+  private def candidatePositions(cells: Set[Cell]): Set[Cell] =
     cells flatMap {
       cell =>
         for {
           x <- cell.x - 1 to cell.x + 1
           y <- cell.y - 1 to cell.y + 1
-        } yield Position(x, y)
+        } yield Cell(x, y)
     }
 
-  private def liveNeighbours(position: Position, cells: Set[Cell]): Int =
-    (cells filter {
+  private def liveNeighbours(candidate: Cell, cells: Set[Cell]): Set[Cell] =
+    cells filter {
       cell =>
-        !(cell.x == position.x && cell.y == position.y) &&
-        cell.x >= position.x - 1 && cell.x <= position.x + 1 &&
-        cell.y >= position.y - 1 && cell.y <= position.y + 1
-    }).size
+        !(cell.x == candidate.x && cell.y == candidate.y) &&
+        cell.x >= candidate.x - 1 && cell.x <= candidate.x + 1 &&
+        cell.y >= candidate.y - 1 && cell.y <= candidate.y + 1
+    }
 
   def tick(cells: Set[Cell]): Set[Cell] =
     candidatePositions(cells) filter {
-      pos =>
-        val n = liveNeighbours(pos, cells)
-        cells find (cell => cell.x == pos.x && cell.y == pos.y) match {
+      candidate =>
+        val n = liveNeighbours(candidate, cells).size
+        cells find (c => c.x == candidate.x && c.y == candidate.y) match {
           case None => n == 3
           case Some(cell) => n >= 2 && n <= 3
         }
-    } map (pos => Cell(pos.x, pos.y))
-    
+    }
 }
