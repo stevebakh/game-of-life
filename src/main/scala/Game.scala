cf3db5e38bd3cc462e6ef8a1002b3482493c1b1a
Steven Bakhtiari <steven.bakh@gmail.com>
2015-07-08 02:00:01 +0100

    Add helper methods to game canvas

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -18,19 +18,40 @@ object Game extends JFXApp {
 
     val CellSize = 10
     val gc = graphicsContext2D
+    var cells: Set[(Long, Long)] = Set.empty
     gc.fill = Color.DarkGray
+
+    def cellCoords: Set[(Long, Long)] = cells
+
+    def clear(): Unit = {
+      gc.clearRect(0, 0, width.value, height.value)
+      cells = Set.empty
+    }
+
+    def plotCell(x: Long, y: Long) = {
+      gc.fillRect(x * CellSize, y * CellSize, CellSize, CellSize)
+      cells = cells + ((x, y))
+    }
+
+    def removeCell(x: Long, y: Long) = {
+      gc.clearRect(x * CellSize, y * CellSize, CellSize, CellSize)
+      cells = cells - ((x, y))
+    }
+
+    def enablePlotting() = addEventHandler(MouseEvent.MOUSE_CLICKED, clickEventHandler)
+    def disablePlotting() = removeEventHandler(MouseEvent.MOUSE_CLICKED, clickEventHandler)
 
     val clickEventHandler = new EventHandler[MouseEvent] {
       override def handle(e: MouseEvent): Unit = {
         val x = e.x - (e.x % CellSize)
         val y = e.y - (e.y % CellSize)
-        gc.fillRect(x, y, CellSize, CellSize)
+        val coords = (x.toLong / CellSize, y.toLong / CellSize)
+        if (cells.contains(coords))
+          removeCell(coords._1, coords._2)
+        else
+          plotCell(coords._1, coords._2)
       }
     }
-
-    def clear(): Unit = gc.clearRect(0, 0, width.value, height.value)
-    def enablePlotting() = addEventHandler(MouseEvent.MOUSE_CLICKED, clickEventHandler)
-    def disablePlotting() = removeEventHandler(MouseEvent.MOUSE_CLICKED, clickEventHandler)
   }
 
   stage = new PrimaryStage {
@@ -77,11 +98,9 @@ object Game extends JFXApp {
         }
 
         center = new Group {
-          // bind the canvas' dimensions to that of the parent's
           gameBoardCanvas.width <== width
           gameBoardCanvas.height <== height
 
-          // set the gameboard canvas as a child of the group
           children = gameBoardCanvas
         }
       }
